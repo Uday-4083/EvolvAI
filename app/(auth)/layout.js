e73b5942d9e78962602ead6d25c617278940705a
Uday <udaygurrapu483@gmail.com>
2025-05-06 23:23:18 +0530

    Add @vercel/analytics dependency and import in AuthLayout component

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,5 +1,6 @@
 const AuthLayout = ({ children }) => {
   return <div className="flex justify-center pt-40">{children}</div>;
 };
+import { Analytics } from "@vercel/analytics/react"
 
 export default AuthLayout;
